8ebe9d2fd8ef31d65fe778c1c08a5bb7ed9e8a18
Fabian Petras <34797639+fpetras@users.noreply.github.com>
2018-05-28 12:02:49 +0200

    Update libft

--- libft/ft_strcasecmp.c
+++ libft/ft_strcasecmp.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strcasecmp.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fpetras <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/07 11:03:09 by fpetras           #+#    #+#             */
-/*   Updated: 2018/03/07 14:17:20 by fpetras          ###   ########.fr       */
+/*   Created: 2017/11/07 15:43:21 by fpetras           #+#    #+#             */
+/*   Updated: 2018/04/13 17:14:37 by fpetras          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int		ft_strcasecmp(const char *s1, const char *s2)
 {
-	size_t len;
+	size_t i;
 
-	len = 0;
-	if (!s)
-		return (len);
-	while (s[len] != '\0')
-		len++;
-	return (len);
+	i = 0;
+	while (ft_tolower(s1[i]) != '\0' && ft_tolower(s2[i]) != '\0' &&
+		ft_tolower(s1[i]) == ft_tolower(s2[i]))
+		i++;
+	return ((unsigned char)ft_tolower(s1[i]) -
+		(unsigned char)ft_tolower(s2[i]));
 }
